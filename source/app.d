a7f58ec1bea9641c490a1c1c8034145a6cd53b11
Alexander Breckel <alexander.breckel@uni-ulm.de>
2017-07-08 00:23:47 +0200

    global variables

--- source/app.d
+++ source/app.d
@@ -38,7 +38,7 @@ final class Reference {
 }
 
 abstract class Expression {
-	abstract Reference evaluate();
+	abstract Reference evaluate(Environment env);
 }
 
 final class IntLiteral : Expression {
@@ -46,8 +46,19 @@ final class IntLiteral : Expression {
 	this(int value) {
 		this.value = value;
 	}
-	override Reference evaluate() {
+	override Reference evaluate(Environment env) {
 		return Reference.RValue(Value.Int(value));
+	}
+}
+
+final class Variable : Expression {
+	string name;
+	this(string name) {
+		this.name = name;
+	}
+	override Reference evaluate(Environment env) {
+		assert(name in env.variables);
+		return Reference.LValue(env.variables[name]);
 	}
 }
 
@@ -57,17 +68,23 @@ final class AddExpression : Expression {
 		this.left  = left;
 		this.right = right;
 	}
-	override Reference evaluate() {
-		auto l = left.evaluate().value;
-		auto r = right.evaluate().value;
+	override Reference evaluate(Environment env) {
+		auto l = left.evaluate(env).value;
+		auto r = right.evaluate(env).value;
 		assert(l.isInt);
 		assert(r.isInt);
 		return Reference.RValue(Value.Int(l.int_ + r.int_));
 	}
 }
 
+final class Environment {
+	Value[string] variables;
+}
+
 void main() {
-	auto expression = new AddExpression(new IntLiteral(19), new IntLiteral(23));
-	auto result     = expression.evaluate();
+	auto expression = new AddExpression(new IntLiteral(19), new Variable("x"));
+	auto env = new Environment;
+	env.variables["x"] = Value.Int(23);
+	auto result = expression.evaluate(env);
 	writeln(result.value);
 }
